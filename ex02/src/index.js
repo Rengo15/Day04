5f03eeefe4c235dcab06471e1b900e69e58eab11
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-11 19:00:24 +0100

    Created ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main (){
+    var myRes = 75;
+    myRes++;
+    // Only change code below this line
+
+    // Only change code above this line
+
+    return myRes;
+}
+
+console.log(main());
+module.exports = main;
