bea51813fecf1400e512308bf6063b60c6794d64
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-12 08:28:41 +0100

    Created ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this line 
+
+    // Only change code above this line 
+    var myDecimal = 8.2;
+    var myDecimalE= 254e-5;
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+
+}
+
+console.log(main());
+module.exports = main;
