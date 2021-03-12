374231937a2944303db2e4908e443680748f9011
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-12 09:12:21 +0100

    Created ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+function main (numOne, numTwo){
+    var numOne;
+    var numTwo;
+    var myQuotient=numOne/numTwo;
+
+
+    // Only change code below this line
+
+    // Only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0,2.0)); //Change this line
+console.log(main(16.5,5.5));
+console.log(main(102.0, 25.5));
+module.exports = main;
