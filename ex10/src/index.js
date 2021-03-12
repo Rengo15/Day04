00e68a3c18167a032263ea05b00af15d65346cf2
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-12 09:59:22 +0100

    Created ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    var myFirstName = "Adnan";
+    var myLastName = "Hamzagic";
+    var myHomeTown = "Sarajevo";
+    // Only change code below this line
+
+    // Only change code above this line
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+
+    };
+}
+console.log(main());
+module.exports = main;
