1202810dacd6304b94ead32c04b9a05554fa5a65
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-12 09:24:17 +0100

    Created ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,16 @@
+function main (numOne, numTwo){
+    var numOne;
+    var numTwo;
+    var remainder=numOne%numTwo;
+
+
+    // Only change code below this line
+
+    // Only change code above this line
+    return remainder;
+}
+
+console.log(main(5, 2)); //Change this line
+console.log(main(15, 4));
+console.log(main(102, 25));
+module.exports = main;
